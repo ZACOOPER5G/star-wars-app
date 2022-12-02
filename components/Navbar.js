43f8d7c214bd88916bc698a100f15dadb6edd1ca
Zac Cooper <zacooper5g@outlook.com>
2022-12-02 20:03:36 +1100

    added navbar styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
+import styles from "../styles/Nav.module.css";
 
 const Navbar = () => {
   return (
     <nav>
-        <div className="logo" >
+        <div className={styles.logo} >
             <h1>Star Wars Flix</h1>
         </div>
-        <Link href="/" className="link">Home</Link>
-        <Link href="/favourites" className="link">Favourites</Link>
-        <Link href="/characters" className="link">Characters</Link>
+        <Link href="/" className={styles.link}>Home</Link>
+        <Link href="/favourites" className={styles.link}>Favourites</Link>
+        <Link href="/characters" className={styles.link}>Characters</Link>
     </nav>
   )
 }
