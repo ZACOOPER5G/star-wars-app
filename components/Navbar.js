ee7e2db1954b9cf746724f634bd7c042d70562bf
Zac Cooper <zacooper5g@outlook.com>
2022-12-03 09:26:15 +1100

    updated nav styling & fonts

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,16 +2,16 @@ import Link from "next/link";
 import styles from "../styles/Nav.module.css";
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div className={styles.logo} >
-            <h1>Star Wars Flix</h1>
-        </div>
-        <Link href="/" className={styles.link}>Home</Link>
-        <Link href="/favourites" className={styles.link}>Favourites</Link>
-        <Link href="/characters" className={styles.link}>Characters</Link>
-    </nav>
-  )
+  	return (
+		<nav>
+			<div className={styles.logo} >
+				<h1 className={styles.logo}>Star Wars Flix</h1>
+			</div>
+			<Link href="/" className={styles.link}>Home</Link>
+			<Link href="/favourites" className={styles.link}>Favourites</Link>
+			<Link href="/characters" className={styles.link}>Characters</Link>
+		</nav>
+	)
 }
 
 export default Navbar
