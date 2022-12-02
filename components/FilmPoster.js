24850bc2a472b91ab54e74a09d38be4304baf499
Zac Cooper <zacooper5g@outlook.com>
2022-12-02 20:44:34 +1100

    added mui card components

--- components/FilmPoster.js
+++ components/FilmPoster.js
@@ -1,8 +1,25 @@
+import { CardContent, Typography, CardActions, Button, Card } from "@mui/material";
 
-
-const FilmPoster = () => {
+const FilmPoster = ({ title, release }) => {
     return (
-        <div>FilmPoster</div>
+        <div>
+            <Card variant="outlined" >
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        { title }
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        adjective
+                    </Typography>
+                    <Typography variant="body2">
+                        { release }
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </div>
     )
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
-import Layout from '../components/Layout'
-import '../styles/globals.css'
+import Layout from '../components/Layout';
+import '../styles/global.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+	return (
+		<Layout>
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</ThemeProvider>
+		</Layout>
+	)
 }
 
 export default MyApp
