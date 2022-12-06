3eef4f00c4d91a5f858a447942d12be022c2099c
Zac Cooper <zacooper5g@outlook.com>
2022-12-06 19:15:56 +1100

    made navbar collapsable/responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,46 @@
 import Link from "next/link";
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
 import styles from "../styles/Nav.module.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { useRef, useState } from "react";
 
 const Navbar = ({ setSearch }) => {
+	const [open, setOpen] = useState(false);
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+		setOpen(!open);
+	};
+
   	return (
 		<nav>
 			<div className={styles.logo} >
 				<h1 className={styles.logo}>Star Wars Flix</h1>
 			</div>
-			<Paper
-				component="form"
-				sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 220, height: 40 }}
-				>
-				<InputBase
-					sx={{ ml: 1, flex: 1, fontSize: "14px" }}
-					placeholder="Search Films"
-					inputProps={{ 'aria-label': 'search google maps' }}
-					onChange={(e) => setSearch(e.target.value)}
-				/>
-				<IconButton type="button" sx={{ p: '10px' }} aria-label="search" >
-					<SearchIcon />
-				</IconButton>
-				</Paper>
-			<Link href="/" className={styles.link}>Home</Link>
-			<Link href="/favourites" className={styles.link}>Favourites</Link>
+			<div className={styles.navLinks} ref={navRef}>
+				<Paper
+					onSubmit={(e) => {
+						e.preventDefault()
+					}}
+					component="form"
+					sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 220, height: 40 }}
+					>
+					<InputBase
+						sx={{ ml: 1, flex: 1, fontSize: "14px" }}
+						placeholder="Search Films"
+						onChange={(e) => {
+							setSearch(e.target.value);
+						}}
+
+					/>
+					</Paper>
+				<Link href="/" className={styles.link}>Home</Link>
+				<Link href="/favourites" className={styles.link}>Favourites</Link>
+				</div>
+				<button className={`${styles.hamburger} nav-btn nav-close-btn`} onClick={showNavbar} >{!open ? <MenuIcon></MenuIcon> : <CloseIcon></CloseIcon>}</button>
 		</nav>
 	)
 }
